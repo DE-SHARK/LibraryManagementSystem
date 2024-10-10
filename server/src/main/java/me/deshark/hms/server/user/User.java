69dd155d05a00c1e24e5848f5326d0886a711ac7
DE_SHARK <2799045359@qq.com>
2024-10-10 18:24:16 +0800

    add lombok

--- server/src/main/java/me/deshark/hms/server/user/User.java
+++ server/src/main/java/me/deshark/hms/server/user/User.java
@@ -1,34 +1,14 @@
 package me.deshark.hms.server.user;
 
+import lombok.Data;
+
+@Data
 public class User {
 
     private Long id;
 
-    private String password;
-
     private String username;
 
-    public Long getId() {
-        return id;
-    }
+    private String password;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
